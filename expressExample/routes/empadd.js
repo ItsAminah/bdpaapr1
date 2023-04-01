6146549bafe8ecf7cd0c08c92a6501840d8eb76a
Amina <100482564+ItsAminah@users.noreply.github.com>
2023-04-01 18:18:27 +0000

    updates4/1

--- expressExample/routes/empadd.js
+++ expressExample/routes/empadd.js
@@ -6,6 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('empadd', { title: 'Products Are Us Employee Add' });
 });
 
+router.post('/', function(req, res, next) {
+  req.body.createdBy="bdpa_adminamina";
+  req.body.modifiedBy="bdpa_adminamina";
+  const httpRequest = require('http');
 
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer' + global.DB_token,
+      'content-type':'application/json'
+    }};
+
+    
+ const data=JSON.stringify(req.body);
+
+    const request = httpRequest.request('http://bdpamkedev.com/api/v5/employees', options, response => {
+          console.log('Status', response.statusCode);
+          //console.log('Headers', response.headers);
+});
+request.on('error', error => console.log('ERROR', error));
+
+request.write(data);
+request.end();
+  res.render('empadd', {title: 'Employee Added!'});
+});
 
 module.exports = router;
